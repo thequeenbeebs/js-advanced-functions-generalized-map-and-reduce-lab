12da28edc737645c0ef774df9ce3a79e6f6eb9de
Blaire Baker <bakerblaire@gmail.com>
2021-01-15 09:51:46 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+
+
+function map(sourceArray, func) {
+    return sourceArray.map(num => func(num))
+}
+
+function reduce(sourceArray, func, startingPoint="") {
+    if (startingPoint) {
+        return sourceArray.reduce(func, startingPoint)
+    }
+    else {
+        return sourceArray.reduce(func)
+    }
+    
+}
